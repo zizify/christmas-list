103e835ce457b3573623d9ea0d38b8ce1461dbc4
Andy Gaines <andyagaines@gmail.com>
2017-11-08 10:54:09 -0600

    modded app and added form and output

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Form from './form';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {list: [
+      {name: 'Angela', gifts: ['sweater', 'cat poster']},
+      {name: 'Dwight', gifts: ['beet juicer', 'water backpack']},
+      {name: 'Jim', gifts: ['personalized comic book']},
+      {name: 'Michael', gifts: ['magic set', 'iPod', 'teapot']}
+  ]};
+
   }
   render() {
+    const list = this.state.list;
+
     return (
-      <div></div>
+      <Form />
     );
   }
 }
